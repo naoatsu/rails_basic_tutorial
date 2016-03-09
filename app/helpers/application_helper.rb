0fdba8b3f40d97a269c07f6cfc04ca03caf1e0ff
kurata <noat11111111@gmail.com>
2016-03-10 01:19:23 +0900

    finish Chapter6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
   def menu_link_to(text, path)
     link_to_unless_current(text, path) {content_tag(:span, text) }
   end
+
+  def add_date_unit(str)
+    if str =~ /(<select.+?<\/select>).+?(<select.+?<\/select>).+?(<select.+?<\/select>)/m
+      ($1 + "年\n" + $2 + "月\n" + $3 + "日\n" + $').html_safe
+    else
+      str
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as: "about"
-
+  get "locale" => "top#locale", as: "locale"
   resources :members do
     collection { get "search" }
   end
